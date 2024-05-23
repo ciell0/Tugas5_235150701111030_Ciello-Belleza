081a28b1a0cfc54333b61bbfc690b660ba323d3c
ciello <161020829+ciell0@users.noreply.github.com>
2024-05-23 16:52:15 +0700

    Add files via upload

---
+++ gui/Tugas5GUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas5.gui;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Tugas5GUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
